b2fb02cd19d182135b0686cf95b9be47ead40d9f
Epaul2390 <epaul2390@gmail.com>
2024-11-15 06:21:05 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("user-menu-button").addEventListener("click", function () {
+    document.getElementById("menu-dropdown").classList.toggle("hidden");
+});
+
+document.getElementById("mobile-menu-dropdown").addEventListener("click", function () {
+    document.getElementById("mobile-menu-dropdown-list").classList.toggle("hidden");
+});
